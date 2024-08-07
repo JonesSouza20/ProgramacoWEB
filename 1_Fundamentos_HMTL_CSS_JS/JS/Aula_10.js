83c84410bf5e020626185ec7523ed66e22a94b38
JonesSouza20 <jonessouza1999@gmail.com>
2024-08-07 07:22:13 -0300

    Create Aula_10.js

---
+++ 1_Fundamentos_HMTL_CSS_JS/JS/Aula_10.js
@@ -0,0 +1,28 @@
+let a = 12;
+let b = 20;
+
+console.log("Soma: ", a + b);
+console.log("Subtração: ", a - b);
+console.log("Divisão: ", a / b);
+console.log("Multiplicação: ", a * b);
+console.log("Módulo: ", a % b);
+
+console.log("Igual: ", a == b);
+console.log("Estritamente igual: ", a === b);
+console.log("Diferente: ", a != b);
+console.log("Estritamente diferente: ", a !== b);
+
+console.log("Maior que: ", a > b);
+console.log("Maior ou igual: ", a >= b);
+console.log("Menor: ", a < b);
+console.log("Menor ou igual: ", a <= b);
+
+console.log("E: ", a && b);
+console.log("OU: ", a || b);
+console.log ("Não: ", !a)
+
+if (a > 12){
+    console.log("Verdadeiro");
+} else{
+    console.log("Falso");
+}
