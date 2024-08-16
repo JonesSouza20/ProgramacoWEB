506018d6b0dadb3279ecb3316321e92f4a3d05e2
JonesSouza20 <jonessouza1999@gmail.com>
2024-08-16 19:18:42 -0300

    Grammar correction in exercise 11

--- 1_Fundamentos_HMTL_CSS_JS/JS/Aula_11.js
+++ 1_Fundamentos_HMTL_CSS_JS/JS/Aula_11.js
@@ -1,8 +1,8 @@
 function peopleAge (age) {
 
 if(age <= 12){
-    return("This person is a chield")
-} 
+    return("This person is a child")
+}
     if(age <= 18){
         return("This person is a teenager ")
 }   if(age > 18 && age <= 65){
