ed2fbcd8cafbbc6536c68dbd4cacc84017ed81bc
JonesSouza20 <jonessouza1999@gmail.com>
2024-09-18 21:42:58 -0300

    Exercicio aula 16

--- 2_JS_Avancado_Desenvolvimento/Exercicio_Aula_16.js
+++ 2_JS_Avancado_Desenvolvimento/Exercicio_Aula_16.js
@@ -1,45 +1,13 @@
-/*function operacaoDemorada(){
-    return new Promise(function(resolve, reject){
-        setTimeout (function(){
-            let sucesso = true;
-            if (sucesso){
-                resolve ("Operação concluida");
-            } else {
-                reject ("Operação não concluida");
-            }
-        }, 5000)
-    })
-}
-
-operacaoDemorada()
-    .then(function(mensagem){
-        console.log(mensagem);
-    })
-    .catch(function(erro){
-        console.log(erro);
-    });
-
-async function executarOperacao() {
-    try{
-        let resultado = await operacaoDemorada();
-        console.log(resultado)
-    } catch(erro){
-        console.log(erro)
-    }
-}
-
-executarOperacao(); */
-
-async function carregarDados() {
-    let resposta =  await fetch('https://jsonplaceholder.typicode.com/posts');
-    let dados =  await resposta.json();
-    try{
+async function dadosAPI(){
+    let resposta = await fetch('https://jsonplaceholder.typicode.com/comments');
+    let resultado = await resposta.json();
+    try {
         resposta;
-        dados;
-        console.log(dados)
-    } catch (erro){
-        console.log("Erro ao carregar dados: ", erro);
+        resultado;
+        console.log(resultado)
+    } catch (erro) {
+        console.log("Não foi possível carregar os dados da API. ", erro);
     }
 }
 
-carregarDados();
+dadosAPI();
