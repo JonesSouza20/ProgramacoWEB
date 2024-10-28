6c714f50974f3801b45298542eece186e936dbd2
JonesSouza20 <jonessouza1999@gmail.com>
2024-10-28 16:19:11 -0300

    Updates

---
+++ 2_JS_Avancado_Desenvolvimento/Exercício_Aula_19/calculadora/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Calculadora from './Calculadora';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Calculadora</h1>
+      <calculadora />
+    </div>
+  );
+}
+
+export default App;
