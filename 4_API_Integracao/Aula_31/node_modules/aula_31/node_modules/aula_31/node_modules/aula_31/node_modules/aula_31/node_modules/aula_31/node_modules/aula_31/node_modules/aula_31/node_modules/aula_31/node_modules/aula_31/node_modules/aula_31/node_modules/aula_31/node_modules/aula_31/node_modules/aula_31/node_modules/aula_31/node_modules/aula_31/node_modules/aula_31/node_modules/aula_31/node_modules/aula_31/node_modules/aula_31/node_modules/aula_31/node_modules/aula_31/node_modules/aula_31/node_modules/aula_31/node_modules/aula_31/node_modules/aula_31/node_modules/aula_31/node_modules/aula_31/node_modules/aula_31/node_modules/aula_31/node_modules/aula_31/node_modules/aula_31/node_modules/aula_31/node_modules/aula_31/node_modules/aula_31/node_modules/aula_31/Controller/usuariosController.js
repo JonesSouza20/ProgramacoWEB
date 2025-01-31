cd1dbb13299995d292bbb852445a2190ea58c31b
JonesSouza20 <jonessouza1999@gmail.com>
2025-01-31 11:17:02 -0300

    Adicionando comentarios ao código

--- 4_API_Integracao/Aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/Controller/usuariosController.js
+++ 4_API_Integracao/Aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/node_modules/aula_31/Controller/usuariosController.js
@@ -1,65 +1,65 @@
-const fs = require ('fs');
-const path = requeire('path');
-const usuariosFilePath = path.json(__dirname, '../Data/usuarios.json');
+const fs = require ('fs'); //Importando o módulo fs que permite manipular arquivos no sistema.
+const path = requeire('path'); //importando o módulo path que permite manipular caminhos de arquivos.
+const usuariosFilePath = path.json(__dirname, '../Data/usuarios.json'); //Caminho do arquivo usuarios.json
 
-const lerUsuarios = () => {
-    const data = fs.readFileSync(usuariosFilePath, 'utf8');
-    return JSON.parse(data)
+const lerUsuarios = () => {  //Função que lê o arquivo usuarios.json
+    const data = fs.readFileSync(usuariosFilePath, 'utf8'); //Lendo o arquivo usuarios.json
+    return JSON.parse(data) //Retornando o arquivo usuarios.json em formato JSON
 };
 
-const salvarUsuarios = (usuarios) => {
-    fs.writeFileSync(usuariosFilePath, JSON.stringify(usuarios, null, 2), 'utf8')
+const salvarUsuarios = (usuarios) => { //Função que salva os dados no arquivo usuarios.json
+    fs.writeFileSync(usuariosFilePath, JSON.stringify(usuarios, null, 2), 'utf8') //Salvando os dados no arquivo usuarios.json
 };
 
-const listarUsuarios = (req, res) => {
-    try {
-        const usuarios = lerUsuarios();
-        res.status(200).json(usuarios);
-    } catch (error) {  
-        res.status(500).json({error: 'Erro ao ler dados'});
+const listarUsuarios = (req, res) => { //Função que lista os usuários
+    try { //Tentando executar o código
+        const usuarios = lerUsuarios(); //Lendo os usuários
+        res.status(200).json(usuarios); //Retornando os usuários
+    } catch (error) {   //Se houver erro
+        res.status(500).json({error: 'Erro ao ler dados'}); //Retorna a mensagem de erro
     }
 };
 
-const criarUsuario = (req, res) => {
-    const {nome, email} = req.body;
+const criarUsuario = (req, res) => { //Função que cria um usuário
+    const {nome, email} = req.body; //Recebendo o nome e email do usuário
 
-    if (!nome || nome.length < 3) {
-        return res.status(400).json({error: 'O nome deve ser obrigatório ou no minimo três digitos'});
+    if (!nome || nome.length < 3) { //Se o nome não for informado ou tiver menos de 3 caracteres
+        return res.status(400).json({error: 'O nome deve ser obrigatório ou no minimo três digitos'}); //Retorna a mensagem de erro
     }
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //Expressão regular para validar o email
     
-    if (!email || !emailRegex.test(email)) {
-        return res.status(400).json({error: 'Email obrigatório'});
+    if (!email || !emailRegex.test(email)) {    //Se o email não for informado ou não for válido
+        return res.status(400).json({error: 'Email obrigatório'});  //Retorna a mensagem de erro
     }
 
-    try {
-        const usuarios = lerUsuarios();
-        const novoUsuario = {id: usuarios.length + 1, nome, email};
-        usuarios.push(novoUsuario);
-        salvarUsuarios(usuarios);
-        res.status(201).json(novoUsuario);
-    } catch (error) {
-        res.status(500).json({error: 'Usuário não adicionado'});
+    try {   //Tentando executar o código
+        const usuarios = lerUsuarios(); //Lendo os usuários
+        const novoUsuario = {id: usuarios.length + 1, nome, email}; //Criando um novo usuário
+        usuarios.push(novoUsuario); //Adicionando o novo usuário no array de usuários
+        salvarUsuarios(usuarios); //Salvando os usuários
+        res.status(201).json(novoUsuario); //Retornando o novo usuário criado com o status 201 (Created) 
+    } catch (error) { //Se houver erro na execução do código acima 
+        res.status(500).json({error: 'Usuário não adicionado'}); //Retorna a mensagem de erro 
     }
 };
 
-const atualizarUsuario = (req, res) => {
-    const {id} = req.params;
-    const {nome, email} = req.body;
+const atualizarUsuario = (req, res) => { //Função que atualiza um usuário 
+    const {id} = req.params; //Recebendo o id do usuário a ser atualizado 
+    const {nome, email} = req.body; //Recebendo o nome e email do usuário a ser atualizado 
 
-    try {
-        const usuarios = lerUsuarios();
-        const usuario = usuarios.find(u=>u.id === parseInt(id));
+    try { //Tentando executar o código 
+        const usuarios = lerUsuarios(); //Lendo os usuários 
+        const usuario = usuarios.find(u=>u.id === parseInt(id)); //Procurando o usuário pelo id 
 
-        if (!usuario) {
-            return res.status(404).json({error: 'Usuario não encontrado'});
+        if (!usuario) { //Se o usuário não for encontrado 
+            return res.status(404).json({error: 'Usuario não encontrado'}); //Retorna a mensagem de erro 
         }
-        usuario.nome = nome || usuario.nome;
-        usuario.email = email || usuario.email;
-        salvarUsuarios(usuarios);
-        res.status(200).json(usuario);
-    } catch (error) {
-        res.status(500).json({error: 'Erro ao atualizar'});
+        usuario.nome = nome || usuario.nome; //Atualizando o nome do usuário 
+        usuario.email = email || usuario.email; //Atualizando o email do usuário
+        salvarUsuarios(usuarios); //Salvando os usuários 
+        res.status(200).json(usuario); //Retornando o usuário atualizado 
+    } catch (error) { //Se houver erro na execução do código acima
+        res.status(500).json({error: 'Erro ao atualizar'}); //Retorna a mensagem de erro 
     }
 };
